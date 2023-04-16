288f7702ca76e957c16b5365cf181de79f9f0794
fakewingmaker <98442068+fakewingmaker@users.noreply.github.com>
2023-04-15 23:28:49 -0400

    updating animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,6 @@
-// function openPopup() {
-//     // Open the pop-up window
-//     var popup = window.open("https://www.example.com", "popupWindow", "width=500,height=500");
-
-//     // Create a button to close the pop-up window
-//     var closeButton = document.createElement("button");
-//     closeButton.innerHTML = "Close pop-up window";
-//     closeButton.addEventListener("click", function() {
-//         popup.close();
-//     });
-
-//     // Add the button to the pop-up window
-//     popup.document.body.appendChild(closeButton);
-// }
 
 //Select the page as document body, and understand it scroll. When the image shows up at a threshold, it shows the pop-up.
+
 function handlePopup() {
     const popup = document.querySelector(".popup")
     const image = document.querySelector('.img')
@@ -28,7 +15,7 @@ function handlePopup() {
                 }
             )
         }, {
-            threshold: 0.2
+            threshold: 0.15
         }
     )
     if(image) observer.observe(image)
@@ -51,4 +38,84 @@ window.onload = function(){
     handlePuzzle()
 
 }
+
+
+
+//the code from gpt, just trying to see the function
+
+		// function handlePopup() {
+		// 	const popup = document.querySelector(".popup")
+		// 	const image = document.querySelector('.img')
+		// 	const observer = new IntersectionObserver(
+		// 		function(imgs) {
+		// 			imgs.forEach(
+		// 				function(img) {
+		// 					if(img.isIntersecting) {
+		// 						popup.removeAttribute("hidden")
+		// 						document.body.setAttribute("no-scroll", true)
+		// 					}
+		// 				}
+		// 			)
+		// 		}, {
+		// 			threshold: 0.2
+		// 		}
+		// 	)
+		// 	if(image) observer.observe(image)
+		// }
+
+		// function handlePuzzle() {
+		// 	const popup = document.querySelector(".popup")
+		// 	const icon1 = new image();
+		// 	icon1.src = "../img/icon1.jpg";
+		// 	image1.style.position = "absolute";
+		// 	image1.style.left = "50px";
+		// 	image1.style.top = "50px";
+		// 	image1.addEventListener("mousedown", dragStart);
+		// 	popup.appendChild(image1);
+		// 	const image2 = new Image();
+		// 	image2.src = "../img/icon2.jpg";
+		// 	image2.style.position = "absolute";
+		// 	image2.style.left = "200px";
+		// 	image2.style.top = "100px";
+		// 	image2.addEventListener("mousedown", dragStart);
+		// 	popup.appendChild(image2);
+
+		// 	function dragStart(event) {
+		// 		var offsetX = event.clientX - event.target.offsetLeft;
+		// 		var offsetY = event.clientY - event.target.offsetTop;
+		// 		document.addEventListener("mousemove", drag);
+		// 		document.addEventListener("mouseup", dragEnd);
+		// 		function drag(event) {
+		// 			event.preventDefault();
+		// 			event.stopPropagation();
+		// 			event.target.style.left = (event.clientX - offsetX) + "px";
+		// 			event.target.style.top = (event.clientY - offsetY) + "px";
+		// 		}
+		// 		function dragEnd(event) {
+		// 			document.removeEventListener("mousemove", drag);
+		// 			document.removeEventListener("mouseup", dragEnd);
+		// 			const target = event.target;
+		// 			const targetRect = target.getBoundingClientRect();
+		// 			const targetCenterX = targetRect.left + targetRect.width / 2;
+		// 			const targetCenterY = targetRect.top + targetRect.height / 2;
+		// 			const button = document.querySelector('button');
+		// 			const buttonRect = button.getBoundingClientRect();
+		// 			if (
+		// 				targetCenterX >= buttonRect.left &&
+		// 				targetCenterX <= buttonRect.right &&
+		// 				targetCenterY >= buttonRect.top &&
+		// 				targetCenterY <= buttonRect.bottom
+		// 			) {
+		// 				popup.setAttribute("hidden", true);
+		// 				document.body.removeAttribute("no-scroll");
+		// 			}
+		// 		}
+		// 	}
+
+		// }
+
+		// window.onload = function(){
+		// 	handlePopup();
+		// 	handlePuzzle();
+		// }
 
