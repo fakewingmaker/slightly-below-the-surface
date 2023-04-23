9f2499ba48ab7010eb0feeaede5c968b4086cf5d
fakewingmaker <98442068+fakewingmaker@users.noreply.github.com>
2023-04-23 13:32:27 -0400

    interactive adjustment

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,24 @@
 
 function handlePopup() {
     const popup = document.querySelector(".popup")
-    const image = document.querySelector('.img')
+    const images = document.querySelectorAll('.img')
     const observer = new IntersectionObserver(
         function(imgs) {
             imgs.forEach(
                 function(img) {
                     if(img.isIntersecting) {
                         popup.removeAttribute("hidden")
+						popup.setAttribute("data-puzzle", img.target.id)
                         document.body.setAttribute("no-scroll", true)
+						observer.unobserve(img.target)
                     }
                 }
             )
         }, {
-            threshold: 0.15
+            threshold: 0.1
         }
     )
-    if(image) observer.observe(image)
+    if(images.length) images.forEach(img=> observer.observe(img))
 }
 
 function handlePuzzle() {
@@ -26,6 +28,12 @@ function handlePuzzle() {
     popup.addEventListener("click", function(){
         popup.setAttribute("hidden", true)
         document.body.removeAttribute("no-scroll")
+		const imgId = popup.getAttribute("data-puzzle")
+		document.getElementById(imgId).scrollIntoView({
+			behavior: "smooth",
+			block: "center",
+		})
+
     })
 
 }
@@ -41,81 +49,4 @@ window.onload = function(){
 
 
 
-//the code from gpt, just trying to see the function
-
-		// function handlePopup() {
-		// 	const popup = document.querySelector(".popup")
-		// 	const image = document.querySelector('.img')
-		// 	const observer = new IntersectionObserver(
-		// 		function(imgs) {
-		// 			imgs.forEach(
-		// 				function(img) {
-		// 					if(img.isIntersecting) {
-		// 						popup.removeAttribute("hidden")
-		// 						document.body.setAttribute("no-scroll", true)
-		// 					}
-		// 				}
-		// 			)
-		// 		}, {
-		// 			threshold: 0.2
-		// 		}
-		// 	)
-		// 	if(image) observer.observe(image)
-		// }
-
-		// function handlePuzzle() {
-		// 	const popup = document.querySelector(".popup")
-		// 	const icon1 = new image();
-		// 	icon1.src = "../img/icon1.jpg";
-		// 	image1.style.position = "absolute";
-		// 	image1.style.left = "50px";
-		// 	image1.style.top = "50px";
-		// 	image1.addEventListener("mousedown", dragStart);
-		// 	popup.appendChild(image1);
-		// 	const image2 = new Image();
-		// 	image2.src = "../img/icon2.jpg";
-		// 	image2.style.position = "absolute";
-		// 	image2.style.left = "200px";
-		// 	image2.style.top = "100px";
-		// 	image2.addEventListener("mousedown", dragStart);
-		// 	popup.appendChild(image2);
-
-		// 	function dragStart(event) {
-		// 		var offsetX = event.clientX - event.target.offsetLeft;
-		// 		var offsetY = event.clientY - event.target.offsetTop;
-		// 		document.addEventListener("mousemove", drag);
-		// 		document.addEventListener("mouseup", dragEnd);
-		// 		function drag(event) {
-		// 			event.preventDefault();
-		// 			event.stopPropagation();
-		// 			event.target.style.left = (event.clientX - offsetX) + "px";
-		// 			event.target.style.top = (event.clientY - offsetY) + "px";
-		// 		}
-		// 		function dragEnd(event) {
-		// 			document.removeEventListener("mousemove", drag);
-		// 			document.removeEventListener("mouseup", dragEnd);
-		// 			const target = event.target;
-		// 			const targetRect = target.getBoundingClientRect();
-		// 			const targetCenterX = targetRect.left + targetRect.width / 2;
-		// 			const targetCenterY = targetRect.top + targetRect.height / 2;
-		// 			const button = document.querySelector('button');
-		// 			const buttonRect = button.getBoundingClientRect();
-		// 			if (
-		// 				targetCenterX >= buttonRect.left &&
-		// 				targetCenterX <= buttonRect.right &&
-		// 				targetCenterY >= buttonRect.top &&
-		// 				targetCenterY <= buttonRect.bottom
-		// 			) {
-		// 				popup.setAttribute("hidden", true);
-		// 				document.body.removeAttribute("no-scroll");
-		// 			}
-		// 		}
-		// 	}
-
-		// }
-
-		// window.onload = function(){
-		// 	handlePopup();
-		// 	handlePuzzle();
-		// }
 
