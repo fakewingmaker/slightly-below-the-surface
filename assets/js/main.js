5a8df4fd02b05af2d75078bf09523d19c5fee632
fakewingmaker <98442068+fakewingmaker@users.noreply.github.com>
2023-04-27 22:29:14 -0400

    updating the javascript to have icon image popup and css change to make it float

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,66 @@
-
-//Select the page as document body, and understand it scroll. When the image shows up at a threshold, it shows the pop-up.
-
 function handlePopup() {
-    const popup = document.querySelector(".popup")
-    const images = document.querySelectorAll('.img')
+    const popup = document.querySelector(".popup");
+    const images = document.querySelectorAll('.img');
+
     const observer = new IntersectionObserver(
-        function(imgs) {
-            imgs.forEach(
-                function(img) {
-                    if(img.isIntersecting) {
-                        popup.removeAttribute("hidden")
-						popup.setAttribute("data-puzzle", img.target.id)
-                        document.body.setAttribute("no-scroll", true)
-						observer.unobserve(img.target)
+        function (images) {
+            images.forEach(
+                function (image) {
+                    if (image.isIntersecting) {
+                        const iconImage = image.target.getAttribute('data-images');
+                        updatePopupImage(popup, iconImage);
+                        popup.removeAttribute("hidden");
+                        popup.setAttribute("data-puzzle", image.target.id);
+                        document.body.setAttribute("no-scroll", true);
+                        observer.unobserve(image.target);
                     }
                 }
-            )
+            );
         }, {
             threshold: 0.075
         }
-    )
-    if(images.length) images.forEach(img=> observer.observe(img))
+    );
+    if (images.length) images.forEach(img => observer.observe(img));
+}
+
+function updatePopupImage(popup, image) {
+    popup.innerHTML = '';
+    const img = document.createElement('img');
+    img.src = image;
+    img.alt = '';
+    img.classList.add('icon');
+    popup.appendChild(img);
 }
 
 function handlePuzzle() {
-    const popup = document.querySelector(".popup")
-    popup.addEventListener("click", function(){
-        popup.setAttribute("hidden", true)
-        document.body.removeAttribute("no-scroll")
-		const imgId = popup.getAttribute("data-puzzle")
-		document.getElementById(imgId).scrollIntoView({
-			behavior: "smooth",
-			block: "center",
-		})
+    const popup = document.querySelector(".popup");
+    popup.addEventListener("click", function () {
+        popup.setAttribute("hidden", true);
+        document.body.removeAttribute("no-scroll");
+        const imgId = popup.getAttribute("data-puzzle");
+        document.getElementById(imgId).scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+        });
 
-    })
-
-}
-
-
-
-
-window.onload = function(){
-    handlePopup()
-    handlePuzzle()
+    });
 
 }
 
+function startRandomFloat(element, duration) {
+    const float = () => {
+        const randomTop = Math.random() * 100;
+        const randomLeft = Math.random() * 100;
+        element.style.top = `${randomTop}%`;
+        element.style.left = `${randomLeft}%`;
+    };
 
+    float();
+    setInterval(float, duration);
+}
 
+window.onload = function () {
+    handlePopup();
+    handlePuzzle();
+};
 
